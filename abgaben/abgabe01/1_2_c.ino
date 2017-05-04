b069809051f2c63adf7c94b942640835d4499c41
Daniel Schäfer <schaefer.dev@gmail.com>
2017-05-04 13:32:06 +0200

    added ino files for ex1

---
+++ abgaben/abgabe01/1_2_c.ino
@@ -0,0 +1,12 @@
+iint led = 13;
+
+void setup () {
+    pinMode(led ,OUTPUT);
+}
+
+void loop () {
+    digitalWrite(led , HIGH);
+    delay (500);
+    digitalWrite(led , LOW);
+    delay (500);
+}
