07f052bfd67811d3dd2c39d445d0bc8bd7bea430
Daniel Schäfer <schaefer.dev@gmail.com>
2017-06-25 16:56:24 +0200

    Aufgabe2 solved, seems to be working

--- abgaben/abgabe07/Aufgabe2/Aufgabe2.ino
+++ abgaben/abgabe07/Aufgabe2/Aufgabe2.ino
@@ -2,7 +2,7 @@ int lightSensor = 2;
 int tempSensor = 8;
 int listsize = 0;
 long lastmess = 0;
-struct list_node;
+
 struct list_node{
     int temp;
     int bright;
@@ -11,18 +11,27 @@ struct list_node{
     struct list_node *pre;
 };
 
+typedef struct list_node List_node;
+
 struct list_node *head = NULL;
 struct list_node *end = NULL;
+
 void setup(){
     pinMode(lightSensor,INPUT);
     pinMode(tempSensor,INPUT);
+    Serial.begin(9600);
 }
 
 void loop(){
     if(millis()-lastmess>1000){
-        struct list_node *n = malloc(sizeof(struct list_node));
+        List_node *n;
+        n = (struct list_node *)malloc(sizeof(struct list_node));
         n->temp = map(analogRead(tempSensor),0,410,-50,150);
         n->bright = map(analogRead(lightSensor),0,1023,0,100);
+        Serial.print("temp: ");
+        Serial.println(n->temp);
+        Serial.print("light: ");
+        Serial.println(n->bright);
         n->time = millis();
         head->pre = NULL;
         n->next = head;
@@ -30,6 +39,7 @@ void loop(){
             end=n;
         head->pre = n;
         head = n;
+       
         listsize++;
         if(listsize==100){
             struct list_node *del = end;
@@ -38,5 +48,17 @@ void loop(){
             listsize--;
         }
         lastmess=millis();
+        
+        Serial.println("--------------------------------");
+        // Gesamte Liste ausgeben 
+        for (n = head; n != NULL; n = n->next) {
+            Serial.println(n->temp); 
+            Serial.println(n->bright); 
+        }
+        Serial.println("--------------------------------");
+        
+      
     }
+    
+    
 }
