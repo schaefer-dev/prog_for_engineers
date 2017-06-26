02568651c385de38bb0b96f245eaedf1880bb55a
Dominik Weber <weberdom@web.de>
2017-06-26 23:47:22 +0200

    redo malloc

--- abgaben/abgabe07/aufgabe03/aufgabe03.ino
+++ abgaben/abgabe07/aufgabe03/aufgabe03.ino
@@ -64,7 +64,7 @@ void insert_node(struct Node *tree , struct Node *node) {
 
 
 struct Node *create_node(int mnr, char *sname, int sem){
-  struct Node *returnNode = (struct Node*)malloc(sizeof(struct Node));
+  struct Node *returnNode = (struct Node *)malloc(sizeof(struct Node));
   *returnNode = { mnr, *sname, sem, NULL, NULL };
   return returnNode;
 }
@@ -74,7 +74,6 @@ struct Node *Database;
 char * student_name(int x) {
   struct Node *result = find_node(Database, x);
   if (result != NULL)
-    Serial.print(*(result->studentName));
     return result->studentName;
   return NULL;
 }
